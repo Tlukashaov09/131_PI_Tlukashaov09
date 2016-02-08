2aa0d5e50120a5a0e6f3275f9810ce3291d83871
tlukashaov09 <tlukashaov09@mail.ru>
2016-02-08 12:38:16 +0300

    Первый комит

--- PI-Lab1/Program.cs
+++ PI-Lab1/Program.cs
@@ -10,6 +10,45 @@ namespace PI_Lab1
     {
         static void Main(string[] args)
         {
+            // Задание 1. Программа для расчета ЗП.
+
+            int e;
+            int c;
+            d = 0;
+
+             Console.WriteLine('Сумма дневной прибыли');
+            e = Convert.ToInt32(Console.ReadLine());
+
+            if  (e <=1000)
+            {
+                d = 1 / 100 * 5 ;
+                Console.WriteLine('Сумма прибыли равен ');
+                Console.WriteLine(decimal);
+            }
+
+            if (e > 10000)
+            {
+                d = e / 100 * 10 + 500;
+                Console.WriteLine('Сумма прибыли равен');
+                Console.WriteLine (d);
+            }
+               
+            Console.ReadKey();
+            
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         }
     }
 }
